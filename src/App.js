8600425a307b0dcc47e881c8ab63882cc40e1603
lahiru8 <lahiru@smbapps.com.au>
2022-08-12 10:49:01 +0530

    sam-20

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SAM web hosting test 10
+          SAM web hosting test 20
         </p>
         <a
           className="App-link"
